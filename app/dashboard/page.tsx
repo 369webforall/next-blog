5fa85c2f7945107e62654d33c204d235080a2b55
369webforall <369webforall@gmail.com>
2025-04-07 17:57:23 +0545

    project setup

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboard = () => {
+  return <div>page dashboard</div>;
+};
+
+export default Dashboard;
---
+++ components/general/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <nav className="py-5 flex items-center justify-between">
+      <div className="flex items-center gap-6">
+        <Link href="/">
+          <h1 className="text-3xl font-semibold">
+            Next <span className="text-blue-600">Blog</span>
+          </h1>
+        </Link>
+
+        <div className="hidden sm:flex items-center gap-6">
+          <Link
+            href="/"
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+          >
+            Home
+          </Link>
+          <Link
+            href="/dashboard"
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+          >
+            Dashboard
+          </Link>
+        </div>
+      </div>
+
+      <div>3</div>
+    </nav>
+  );
+};
+
+export default Navbar;
